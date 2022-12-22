1281ef2ee4d15b3a2ff015de79ebbc683703603a
melissa <90412072+StamperM@users.noreply.github.com>
2022-12-22 16:41:25 -0500

    added some new functions to make the generation work

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
-
 var generateBtn = document.querySelector("#generate");
 
-var passwordType = prompt("Would you like to use lowercase, uppercase, and special characters?");
-var passwordLength = prompt("Please select a password length between 8 and 128");
-var passwordLength = Number(howLongOfPassword);
+// Prompt user for password length, length has to be 8-128.
+// 
 
-function passwordLengthRequirements(){
-  if (passwordLength < 8 || passwordLength >128){
-  return passwordLength;
-} else {
+function getPasswordLengthFromUser() {
+  var howManyNumbers = prompt(
+    "Please select a password length between 8 and 128"
+  );
+  console.log(howManyNumbers);
+  if (howManyNumbers < 8 || howManyNumbers > 128) {
+    return getPasswordLengthFromUser();
+  } else {
+    return howManyNumbers;
+  }
+}
+
+function getPasswordStyleFromUser() {
+  var passwordType = prompt("Would you like your password to include Uppercase,lowercase and special characters?")
+  if (passwordType = null){
+    return;
+    } else{
   return passwordType;
-}
- 
- 
-  // if (passwordLength > 128 && passwordLength < 8) {
-  //   return  var passwordRetry = y 
-  // } else {
-  //   return 
-
-  // }
-   if (passwordType = null){
-    return 
-   }
-     
-   function createPasswordNumbers(passwordNumber) {
-    var results =''
-    var passwordNumber = '0123456789'
-    for (var i = 0; i < passwordLength; i++)
-    results += characters.charAt(math.floor(Math.random());
-   } 
-   function createPasswordType(passwordType){
-    var results =''
-    var withTypes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789!@#$%&abcdefghijklmnopqrstuvwxyz'
-    for (var i = 0; i < passwordLength; i++)
-    results += characters.charAt(math.floor(Math.random());
-  }
-}
-function generatePassword(){
-console.log("you clicked me");
-passwordRequirements()
-
+    } 
   }
 
+
+
+function createPasswordNumbers(passwordLength) {
+  var results = "";
+  var passwordNumber = "0123456789";
+  for (var i = 1; i < passwordLength ; i++)
+    results += passwordNumber.charAt(Math.floor(Math.random()*passwordLength));
+  return results;
+}
+function createPasswordType(passwordLength, passwordType) {
+  var results = "";
+  var withTypes =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789!@#$%&abcdefghijklmnopqrstuvwxyz";
+  for (var i = 0; i < passwordLength; i++)
+    results += characters.charAt(Math.floor(Math.random()));
+    return results;
 }
 
 
@@ -53,8 +51,15 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+function generatePassword() {
+  console.log("you clicked me");
+  var passwordLength = getPasswordLengthFromUser();
+  createPasswordNumbers(passwordLength);
+  getPasswordStyleFromUser();
+
+  return createPasswordNumbers(passwordLength);
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
